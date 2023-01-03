f67bd558d58c24c6d34822d3e32d5f3c31a7ca96
Hajong Kim <whoami125@g.skku.edu>
2023-01-04 01:22:21 +0900

    first level completed

---
+++ 1단계/2588.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+
+first = int(a)*int(b[0])
+second = int(a)*int(b[1])
+third = int(a)*int(b[2])
+
+print(third)
+print(second)
+print(first)
+print((first*100)+(second*10)+third)
---
+++ 1단계/3003.py
@@ -0,0 +1,3 @@
+k, q, l, b, kn, p = input().split()
+
+print(-(int(k)-1), -(int(q)-1), -(int(l)-2), -(int(b)-2), -(int(kn)-2), -(int(p)-8))
